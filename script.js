441ed58b8997628bc9973f674798b5e90e3004e7
Schugu <47997052+Schugu@users.noreply.github.com>
2024-03-10 20:49:30 -0300

    Refactorización de código.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 import {signosBraille} from 'http://127.0.0.1:5500/signosBraille.js'
 
+const contenedorBotones = document.querySelectorAll(`.alfabeto__signos`);
+const contenedorLetras = document.querySelectorAll(`.alfabeto__letras`);
+    
 function ponerBotones () {
     const botones = document.querySelectorAll(".botonClickeable");
     const parrafo = document.querySelector('.parrafo');
@@ -24,99 +27,39 @@ function ponerBotones () {
         });
 };
 
-
-
-
-
-
-function crearSignos () {
-    const contenedorBotones = document.querySelectorAll(`.alfabeto__signos`);
-    const contenedorLetras = document.querySelectorAll(`.alfabeto__letras`);
-
-    for (let i = 0; i < 10; i++) {
+function crearFilasDeBotones (fila, inicio, final) { 
+    for (let i = inicio; i < final; i++) {
         const signosBrailleLetra = Object.keys(signosBraille)[i];
 
         const divBoton = document.createElement(`div`);
         divBoton.classList.add(`boton`, ('signo' + signosBrailleLetra.toUpperCase()));
-        contenedorBotones[0].appendChild(divBoton);
+        contenedorBotones[fila].appendChild(divBoton);
         divBoton.id = `signoAlfabetoF1${signosBrailleLetra}`;
 
-        const divLetra = document.createElement(`div`);
-        divLetra.classList.add(`alfabeto__letra`);
-        contenedorLetras[0].appendChild(divLetra);
-        const spanLetra = document.createElement(`span`);
-        divLetra.appendChild(spanLetra);
-        spanLetra.textContent = signosBrailleLetra;   
-    }
-    for (let i = 0; i < 10; i++) {
-        const signosBrailleLetra = Object.keys(signosBraille)[i + 10];
-
-        const divBoton = document.createElement(`div`);
-        divBoton.classList.add(`boton`, ('signo' + signosBrailleLetra.toUpperCase()));
-        contenedorBotones[1].appendChild(divBoton);
-        divBoton.id = `signoAlfabetoF2${signosBrailleLetra}`;
-
-        const divLetra = document.createElement(`div`);
-        divLetra.classList.add(`alfabeto__letra`);
-        contenedorLetras[1].appendChild(divLetra);
-        const spanLetra = document.createElement(`span`);
-        divLetra.appendChild(spanLetra);
-        spanLetra.textContent = signosBrailleLetra;   
-    }
-    for (let i = 20; i < 25; i++) {
-        const signosBrailleLetra = Object.keys(signosBraille)[i];
-
-        const divBoton = document.createElement(`div`);
-        divBoton.classList.add(`boton`, ('signo' + signosBrailleLetra.toUpperCase()));
-        contenedorBotones[2].appendChild(divBoton);
-        divBoton.id = `signoAlfabetoF2${signosBrailleLetra}`;
-
-        const divLetra = document.createElement(`div`);
-        divLetra.classList.add(`alfabeto__letra`);
-        contenedorLetras[2].appendChild(divLetra);
-        const spanLetra = document.createElement(`span`);
-        divLetra.appendChild(spanLetra);
-        spanLetra.textContent = signosBrailleLetra;   
-    }
-
-    for (let i = 25; i < 27; i++) {
-        const signosBrailleLetra = Object.keys(signosBraille)[i];
-
-        const divBoton = document.createElement(`div`);
-        divBoton.classList.add(`boton`, ('signo' + signosBrailleLetra.toUpperCase()));
-        contenedorBotones[3].appendChild(divBoton);
-        divBoton.id = `signoAlfabetoF2${signosBrailleLetra}`;
-
-        const divLetra = document.createElement(`div`);
-        divLetra.classList.add(`alfabeto__letra`);
-        contenedorLetras[3].appendChild(divLetra);
-        const spanLetra = document.createElement(`span`);
-        divLetra.appendChild(spanLetra);
-        spanLetra.textContent = signosBrailleLetra;   
-    }          
-        
-
-
-
-    const botones = document.querySelectorAll(".boton");
-    botones.forEach(function(boton){
-        for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 6; j++) {
             const spanSigno = document.createElement("span");
             spanSigno.classList.add("punto");
-            boton.appendChild(spanSigno);
-            spanSigno.id = `punto${i+1}`
+            divBoton.appendChild(spanSigno);
+            spanSigno.id = `punto${j+1}`
         }
-    });
-    
+
+        const divLetra = document.createElement(`div`);
+        divLetra.classList.add(`alfabeto__letra`);
+        contenedorLetras[fila].appendChild(divLetra);
+        const spanLetra = document.createElement(`span`);
+        divLetra.appendChild(spanLetra);
+        spanLetra.textContent = signosBrailleLetra;  
+    }
+} 
+
+function crearSignos () {
+    crearFilasDeBotones(0, 0, 10);
+    crearFilasDeBotones(1, 10, 20);
+    crearFilasDeBotones(2, 20, 25);
+    crearFilasDeBotones(3, 25, 27);
 }
-
-
-
-
 
 document.addEventListener("DOMContentLoaded", function() {
     ponerBotones();
     crearSignos();
-
-
 });

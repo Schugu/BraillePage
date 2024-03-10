3bdfb78d1a5aacddf37e8028a072ceabbc4d6814
Schugu <47997052+Schugu@users.noreply.github.com>
2024-03-10 17:11:13 -0300

    Tratando de mejorar el código
    Ideando formas para tratar de achicar el código HTML por medio de JavaScript.

--- script.js
+++ script.js
@@ -31,8 +31,28 @@ function ponerBotones () {
 
 
 
+
+function crearElementoPrueba () {
+    const pruebaArticle = document.querySelector(".pruebaArticle");
+    const botonPrueba = document.createElement("div");
+
+    for (i = 0; i < 6; i++) {
+        const spanPrueba = document.createElement("span");
+        botonPrueba.appendChild(spanPrueba);
+        spanPrueba.classList.add("punto", "puntoPresente");
+    }
+    
+    pruebaArticle.appendChild(botonPrueba);
+    botonPrueba.classList.add("boton");
+}
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     ponerBotones();
+    // crearElementoPrueba();
 
 
 });

692d4d2757181c7a6e2d62c40a6196df48822d89
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-06 14:34:19 -0300

    Add: Type y Formato

--- src/components/codificador/Signo.tsx
+++ src/components/codificador/Signo.tsx
@@ -1,17 +1,30 @@
 import { letrasBraille } from "./signosBraille.ts";
 
-export default function Signo({ cadenaParaCodificar }) {
+type SignoProps = {
+  cadenaParaCodificar: string,
+  formato: string;
+}
+
+export default function Signo({ cadenaParaCodificar, formato }: SignoProps) {
   const cadenaSeparada = cadenaParaCodificar.toLowerCase().split("");
 
-  // Almacenar los elementos JSX en una variable
-  const elementosBraille = cadenaSeparada.map((caracter, index) => {
+  const elementosBraille = cadenaSeparada.map(caracter => {
     if (caracter in letrasBraille) {
       return (
-        <article key={caracter} className="w-14 h-20 grid grid-rows-3 grid-cols-2 cursor-grab gap-2 p-1.5
-        bg-white hover:bg-gray-200 border border-black shadow-md transition-colors duration-150">
+        <article key={caracter} className={`grid grid-rows-3 grid-cols-2 place-items-center cursor-grab
+        bg-transparent hover:bg-gray-200 hover:bg-opacity-50 border border-black shadow-braille transition-colors duration-150
+          ${formato === 'grande' ? 'w-[100px] h-[147px]' : ''}
+          ${formato === 'mediano' ? 'w-[75px] h-[110.25px]' : ''}
+          ${formato === 'chico' ? 'w-[50px] h-[73.5px]' : ''}
+        `}>
           {
             letrasBraille[caracter].map((punto, puntoIndex) => (
-              <span key={puntoIndex} className={`${punto === 1 ? '' : 'bg-opacity-10'} bg-black rounded-full cursor-grab shadow-md`}></span>
+              <span key={puntoIndex}
+                className={`${punto === 1 ? '' : 'bg-opacity-10'} bg-black rounded-full cursor-grab shadow-braille
+                  ${formato === 'grande' ? 'w-[30px] h-[30px]' : ''}
+                  ${formato === 'mediano' ? 'w-[22.5px] h-[22.5px]' : ''}
+                  ${formato === 'chico' ? 'w-[15px] h-[15px]' : ''}
+              `}></span>
             ))
           }
         </article>

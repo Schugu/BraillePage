5af4139ef938a5181867ec379499212f4291e9af
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-06 10:32:23 -0300

    Add: Medidas y BgColor

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home.tsx";
+import Navbar from "./components/navbar/Navbar.tsx";
 
 export default function App() {
   return (
     <BrowserRouter>
- 
-      
-      <Routes>
-        <Route path='/' element={<Home />} />
 
-      </Routes>
-  
-  </BrowserRouter>
+      <main className='flex flex-col min-h-screen bg-DURAZNO-light'>
+        <Navbar></Navbar>
+        <section className='flex-grow'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+
+          </Routes>
+        </section>
+      </main>
+
+    </BrowserRouter>
   )
 }

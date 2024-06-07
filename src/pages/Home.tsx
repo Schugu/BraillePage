679f9077d09d117c685e5e49febd6e808916b1dc
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-07 09:56:39 -0300

    Refactor: Nuevo componente

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
-import Signo from "../components/codificador/Signo.tsx";
+import SignoBraille from "../components/codificador/SignoBraille.tsx";
+import SignoBrailleConPuntos from "../components/codificador/SignoBrailleConNumeros.tsx";
+
 export default function Home() {
   return (
     <section className="w-full h-full flex flex-col items-center gap-8 p-2">
@@ -6,17 +8,17 @@ export default function Home() {
 
       <article className="flex flex-col gap-1 text-2xl">
         <div className="w-full flex flex-wrap items-center justify-center gap-2">
-          <Signo cadenaParaCodificar={"¥"} formato="grande" />
+          <SignoBraille cadenaParaCodificar={"¥"} formato="grande" />
           <p className="inline-block w-1/3 text-left text-balance self-center">El signo básico del Sistema se denomina SIGNO GENERADOR, y está formado por seis puntos.</p>
         </div>
 
         <div className="w-full flex flex-wrap items-center justify-center gap-2">
-          <Signo cadenaParaCodificar={"Felíz"} formato="grande" />
+          <SignoBraille cadenaParaCodificar={"Felíz"} formato="grande" />
           <p className="inline-block w-1/3 text-left text-balance self-center">Combinando de distintas maneras estos seis puntos, pueden representarse todas las letras, los números, los signos, la musicografía, etc.</p>
         </div>
 
         <div className="w-full flex flex-wrap items-center justify-center gap-2">
-          <Signo cadenaParaCodificar={"¥"} formato="grande" />
+          <SignoBrailleConPuntos cadenaParaCodificar={"¥"} formato="grande" />
           <p className="inline-block w-1/3 text-left text-balance self-center">Para facilitar la identificación de cada punto, cada uno, toma el nombre de un número.</p>
         </div>
       </article>

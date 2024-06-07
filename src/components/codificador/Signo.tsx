a8c7444b1a5d1c3983294e93a31433e0c42868fa
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-07 09:42:58 -0300

    Refactor: Cambio de cursor

--- src/components/codificador/Signo.tsx
+++ src/components/codificador/Signo.tsx
@@ -11,7 +11,7 @@ export default function Signo({ cadenaParaCodificar, formato }: SignoProps) {
   const elementosBraille = cadenaSeparada.map(caracter => {
     if (caracter in letrasBraille) {
       return (
-        <article key={caracter} className={`grid grid-rows-3 grid-cols-2 place-items-center cursor-grab
+        <article key={caracter} className={`grid grid-rows-3 grid-cols-2 place-items-center cursor-pointer
         bg-transparent hover:bg-gray-200 hover:bg-opacity-50 border border-black shadow-braille transition-colors duration-150
           ${formato === 'grande' ? 'w-[100px] h-[147px]' : ''}
           ${formato === 'mediano' ? 'w-[75px] h-[110.25px]' : ''}
@@ -20,7 +20,7 @@ export default function Signo({ cadenaParaCodificar, formato }: SignoProps) {
           {
             letrasBraille[caracter].map((punto, puntoIndex) => (
               <span key={puntoIndex}
-                className={`${punto === 1 ? '' : 'bg-opacity-10'} bg-black rounded-full cursor-grab shadow-braille
+                className={`${punto === 1 ? '' : 'bg-opacity-10'} bg-black rounded-full cursor-pointer shadow-braille
                   ${formato === 'grande' ? 'w-[30px] h-[30px]' : ''}
                   ${formato === 'mediano' ? 'w-[22.5px] h-[22.5px]' : ''}
                   ${formato === 'chico' ? 'w-[15px] h-[15px]' : ''}

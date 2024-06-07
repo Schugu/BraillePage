7a12d30fab439cfa4650ad401bda632d8f9933b3
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-07 09:43:17 -0300

    Add: Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,26 @@
 import Signo from "../components/codificador/Signo.tsx";
 export default function Home() {
   return (
-    <section className="w-full h-full p-2">
-      <Signo cadenaParaCodificar={"b"}/>
+    <section className="w-full h-full flex flex-col items-center gap-8 p-2">
+      <h1 className="w-full border-b-4 border-orange-600 text-5xl text-center">El sistema Braille</h1>
+
+      <article className="flex flex-col gap-1 text-2xl">
+        <div className="w-full flex flex-wrap items-center justify-center gap-2">
+          <Signo cadenaParaCodificar={"¥"} formato="grande" />
+          <p className="inline-block w-1/3 text-left text-balance self-center">El signo básico del Sistema se denomina SIGNO GENERADOR, y está formado por seis puntos.</p>
+        </div>
+
+        <div className="w-full flex flex-wrap items-center justify-center gap-2">
+          <Signo cadenaParaCodificar={"Felíz"} formato="grande" />
+          <p className="inline-block w-1/3 text-left text-balance self-center">Combinando de distintas maneras estos seis puntos, pueden representarse todas las letras, los números, los signos, la musicografía, etc.</p>
+        </div>
+
+        <div className="w-full flex flex-wrap items-center justify-center gap-2">
+          <Signo cadenaParaCodificar={"¥"} formato="grande" />
+          <p className="inline-block w-1/3 text-left text-balance self-center">Para facilitar la identificación de cada punto, cada uno, toma el nombre de un número.</p>
+        </div>
+      </article>
+
     </section>
   )
 }

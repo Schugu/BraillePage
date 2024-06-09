7f041fb9e98ea3f3d5662c4eccd35123bc296104
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-08 23:24:16 -0300

    Add: Importación de componente

--- src/components/codificador/SignoNumBraille.tsx
+++ src/components/codificador/SignoNumBraille.tsx
@@ -1,4 +1,5 @@
-import { numerosBraille } from "./signosBraille.ts";
+import brailleSignValue from "./brailleSignValue.ts";
+import BrailleSign from "../codificador/BrailleSign.tsx";
 
 type SignoBrailleProps = {
   numerosParaCodificar: string,
@@ -8,9 +9,9 @@ type SignoBrailleProps = {
 export default function SignoBraille({ numerosParaCodificar, formato }: SignoBrailleProps) {
   const numeros = numerosParaCodificar.match(/\d+|\s+/g);
   
-  const elementosBraille = numeros.map((numero, numeroIndex) => {
+  const elementosBraille = numeros.map(numero => {
     if (numero.length < 2) {
-      if (numero in numerosBraille) 
+      if (numero in brailleSignValue) 
         console.log(numero);
 
         let numeroConSimbolo = [];
@@ -24,25 +25,9 @@ export default function SignoBraille({ numerosParaCodificar, formato }: SignoBra
         console.log(numeroConSimbolo);
 
         return numeroConSimbolo.map((num, index) => {
-          if (num in numerosBraille) {
+          if (num in brailleSignValue) {
             return (
-              <article key={`${num}-${numeroIndex}-${index}`} className={`grid grid-rows-3 grid-cols-2 place-items-center cursor-pointer rounded
-              bg-transparent hover:bg-gray-200 hover:bg-opacity-50 border border-black shadow-braille transition-colors duration-150
-                ${formato === 'grande' ? 'w-[100px] h-[147px]' : ''}
-                ${formato === 'mediano' ? 'w-[75px] h-[110.25px]' : ''}
-                ${formato === 'chico' ? 'w-[50px] h-[73.5px]' : ''}
-              `}>
-                {
-                  numerosBraille[num].map((punto, puntoIndex) => (
-                    <span key={`${num}-${numeroIndex}-${index}-${puntoIndex}`}
-                      className={`${punto === 1 ? '' : 'bg-opacity-10'} bg-black rounded-full cursor-pointer shadow-braille
-                        ${formato === 'grande' ? 'w-[30px] h-[30px]' : ''}
-                        ${formato === 'mediano' ? 'w-[22.5px] h-[22.5px]' : ''}
-                        ${formato === 'chico' ? 'w-[15px] h-[15px]' : ''}
-                    `}></span>
-                  ))
-                }
-              </article>
+              <BrailleSign caracter={num} formato={formato} index={index}/>
             );
           } else {
             return null;

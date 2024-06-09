ed910aec4a3e5df0e5800e8ff3b169cb7c2293ac
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-08 23:18:38 -0300

    Add: Componente para ahorrar código

---
+++ src/components/codificador/BrailleSign.tsx
@@ -0,0 +1,29 @@
+import brailleSignValue from "./brailleSignValue.ts";
+
+type BrailleSignProps = {
+  caracter: string;
+  formato: string;
+  index: number;
+}
+
+export default function BrailleSign({caracter, formato, index}: BrailleSignProps) {
+  return (
+    <article key={`${caracter}-${index}`} className={`grid grid-rows-3 grid-cols-2 place-items-center cursor-pointer rounded
+      bg-transparent hover:bg-gray-200 hover:bg-opacity-50 border border-black shadow-braille transition-colors duration-150
+        ${formato === 'grande' ? 'w-[100px] h-[147px]' : ''}
+        ${formato === 'mediano' ? 'w-[75px] h-[110.25px]' : ''}
+        ${formato === 'chico' ? 'w-[50px] h-[73.5px]' : ''}
+      `}>
+        {
+          brailleSignValue[caracter].map((punto, puntoIndex) => (
+            <span key={puntoIndex}
+              className={`${punto === 1 ? '' : 'bg-opacity-10'} bg-black rounded-full cursor-pointer shadow-braille
+                ${formato === 'grande' ? 'w-[30px] h-[30px]' : ''}
+                ${formato === 'mediano' ? 'w-[22.5px] h-[22.5px]' : ''}
+                ${formato === 'chico' ? 'w-[15px] h-[15px]' : ''}
+            `}></span>
+          ))
+        }
+      </article>
+  )
+}

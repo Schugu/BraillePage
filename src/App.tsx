0f24a2d0e996800509254939af9d9b7dc090d51f
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-08 22:00:35 -0300

    Add: Numbers Route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./components/header/Header.tsx";
 import Footer from "./components/footer/Footer.tsx";
 import Home from "./pages/Home.tsx";
 import Alphabet from "./pages/Alphabet.tsx";
+import Numbers from "./pages/Numbers.tsx";
 
 
 export default function App() {
@@ -17,6 +18,7 @@ export default function App() {
             <Route path='/' element={<Home />} />
             <Route path="/home" element={<Navigate to="/" />} />
             <Route path='/alphabet' element={<Alphabet />} />
+            <Route path='/numbers' element={<Numbers />} />
 
           </Routes>
         </section>
---
+++ src/pages/Numbers.tsx
@@ -0,0 +1,9 @@
+import SignoNumBraille from "../components/codificador/SignoNumBraille.tsx";
+
+export default function Numbers() {
+  return (
+    <div>
+      <SignoNumBraille formato="chico" numerosParaCodificar="1 2"/>
+    </div>
+  )
+}

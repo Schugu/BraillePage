b9676a1a32ea82b3d8135061b825ab3f29a85835
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-09 00:05:35 -0300

    Add: Lógica del componente de números

--- src/components/codificador/SignoNumBraille.tsx
+++ src/components/codificador/SignoNumBraille.tsx
@@ -8,35 +8,43 @@ type SignoBrailleProps = {
 
 export default function SignoBraille({ numerosParaCodificar, formato }: SignoBrailleProps) {
   const numeros = numerosParaCodificar.match(/\d+|\s+/g);
-  
-  const elementosBraille = numeros.map(numero => {
-    if (numero.length < 2) {
-      if (numero in brailleSignValue) 
-        console.log(numero);
 
+  if (numeros === null) {
+    return null;
+  }
+
+  const elementosBraille = numeros.map(numSinFormato => {
+    const numero = parseFloat(numSinFormato).toLocaleString('es-ES').toString();
+
+    if (numero.length < 2) {
+      if (numero in brailleSignValue) {
         let numeroConSimbolo = [];
 
-        if (numero === ' '){
+        if (numero === ' ') {
           numeroConSimbolo = [numero];
         } else {
           numeroConSimbolo = ['º', numero];
         }
 
-        console.log(numeroConSimbolo);
-
         return numeroConSimbolo.map((num, index) => {
-          if (num in brailleSignValue) {
-            return (
-              <BrailleSign caracter={num} formato={formato} index={index}/>
-            );
-          } else {
-            return null;
-          }
+          return (
+            <BrailleSign caracter={num} formato={formato} index={index} />
+          );
         });
+      }
     } else {
-      return null;
+      const charSeparados = ['º', ...numero.split('')];
+
+      return charSeparados.map((char, index) => {
+        if (char in brailleSignValue) {
+          return (
+            <BrailleSign caracter={char} formato={formato} index={index} key={index} /> // Asegúrate de agregar una key única para cada componente
+          );
+        }
+        return null; 
+      });
     }
-  });
+  })
 
   return (
     <section className="flex flex-wrap gap-1">

1bc6bca2ba1eae4286f2805db5b9c3aecd31d5dd
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-09 00:33:25 -0300

    Refactor: Cambio de medidas y nombres

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import WordToBraille from "../components/codificador/WordToBraille.tsx";
+import CharsToBraille from "../components/codificador/CharsToBraille.tsx";
 import CharToBrailleWithNumbers from "../components/codificador/CharToBrailleWithNumbers.tsx";
 
 export default function Home() {
@@ -8,18 +8,18 @@ export default function Home() {
 
       <article className="flex flex-col gap-1 text-2xl">
         <div className="w-full flex flex-wrap items-center justify-center gap-2">
-          <WordToBraille cadenaParaCodificar={"¥"} formato="grande" />
-          <p className="inline-block w-1/3 text-left text-balance self-center">El signo básico del Sistema se denomina SIGNO GENERADOR, y está formado por seis puntos.</p>
+          <CharsToBraille cadenaParaCodificar={"¥"} formato="grande" />
+          <p className="inline-block max-w-lg text-left text-balance self-center">El signo básico del Sistema se denomina SIGNO GENERADOR, y está formado por seis puntos.</p>
         </div>
 
         <div className="w-full flex flex-wrap items-center justify-center gap-2">
-          <WordToBraille cadenaParaCodificar={"Felíz"} formato="grande" />
-          <p className="inline-block w-1/3 text-left text-balance self-center">Combinando de distintas maneras estos seis puntos, pueden representarse todas las letras, los números, los signos, la musicografía, etc.</p>
+          <CharsToBraille cadenaParaCodificar={"Felíz"} formato="grande" />
+          <p className="inline-block max-w-lg text-left text-balance self-center">Combinando de distintas maneras estos seis puntos, pueden representarse todas las letras, los números, los signos, la musicografía, etc.</p>
         </div>
 
         <div className="w-full flex flex-wrap items-center justify-center gap-2">
           <CharToBrailleWithNumbers cadenaParaCodificar={"¥"} formato="grande" />
-          <p className="inline-block w-1/3 text-left text-balance self-center">Para facilitar la identificación de cada punto, cada uno, toma el nombre de un número.</p>
+          <p className="inline-block max-w-lg text-left text-balance self-center">Para facilitar la identificación de cada punto, cada uno, toma el nombre de un número.</p>
         </div>
       </article>
 

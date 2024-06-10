9aa8b26d939a51e59b1bcdbaa6e3d0ea8b0d0f55
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-10 08:30:30 -0300

    Add: Section WordsToBraille

---
+++ src/pages/WordsToBraille.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import CharsToBraille from "../components/codificador/CharsToBraille";
+
+export default function WordsToBraille() {
+  const [palabra, setPalabra] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPalabra(event.target.value);
+  };
+
+  return (
+    <section className="flex flex-col items-center gap-8 p-2 text-2xl">
+      <h1 className="w-full border-b-4 border-blue-950 text-5xl text-center">
+        Codificador de palabras
+      </h1>
+
+      <article className="flex flex-col items-center gap-2">
+        <p>Escriba una palabra</p>
+        <div className="flex flex-wrap gap-2">
+          <input
+            onChange={handleChange}
+            value={palabra}
+            className="rounded border-2 border-blue-400 p-2 outline-none"
+            type="text"
+            placeholder="Hola"
+            autoComplete="off"
+          />
+          <button
+            onClick={() => { setPalabra('') }}
+            className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+          >Borrar todo</button>
+        </div>
+      </article>
+
+      <article className="w-full flex justify-center">
+        <CharsToBraille cadenaParaCodificar={palabra} formato="chico" />
+      </article>
+    </section>
+  );
+}

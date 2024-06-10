2a72548265343d1cb2f9999ac981453319f350c3
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-09 21:28:41 -0300

    Fix: Bug keys

--- src/components/codificador/CharToBrailleWithChar.tsx
+++ src/components/codificador/CharToBrailleWithChar.tsx
@@ -12,15 +12,15 @@ export default function CharToBrailleWithChar({ cadenaParaCodificar, formato }: 
   const elementosBraille = cadenaSeparada.map((caracter, index) => {
     if (caracter in brailleSignValue) {
       return (
-        <section className="flex flex-col gap-1">
-          <BrailleSign caracter={caracter} formato={formato} key={`${caracter}-${index}`}/>
+        <section key={`${caracter}-${index}`} className="flex flex-col gap-1">
+          <BrailleSign caracter={caracter} formato={formato} key={`braille-${caracter}-${index}`} />
 
-          <article key={caracter} className={`flex justify-center items-center cursor-pointer rounded
-        bg-transparent hover:bg-gray-200 hover:bg-opacity-50 border border-black shadow-braille transition-colors duration-150
-          ${formato === 'grande' ? 'w-[100px] h-[147px] text-8xl' : ''}
-          ${formato === 'mediano' ? 'w-[75px] h-[110.25px] text-7xl' : ''}
-          ${formato === 'chico' ? 'w-[50px] h-[73.5px] text-5xl' : ''}
-        `}>
+          <article key={`article-${caracter}-${index}`} className={`flex justify-center items-center cursor-pointer rounded
+            bg-transparent hover:bg-gray-200 hover:bg-opacity-50 border border-black shadow-braille transition-colors duration-150
+            ${formato === 'grande' ? 'w-[100px] h-[147px] text-8xl' : ''}
+            ${formato === 'mediano' ? 'w-[75px] h-[110.25px] text-7xl' : ''}
+            ${formato === 'chico' ? 'w-[50px] h-[73.5px] text-5xl' : ''}
+          `}>
             <p className="">
               {caracter.toUpperCase()}
             </p>
@@ -31,7 +31,6 @@ export default function CharToBrailleWithChar({ cadenaParaCodificar, formato }: 
       return null;
     }
   });
-
 
   return (
     <section className="flex flex-wrap justify-center gap-1">

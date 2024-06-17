55db8a904f42dae5fd07a2ecdbd77f535a09d0e9
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-17 11:20:12 -0300

    Add: tabIndex y aria-label

--- src/components/codificador/BrailleSign.tsx
+++ src/components/codificador/BrailleSign.tsx
@@ -4,26 +4,44 @@ type BrailleSignProps = {
   caracter: string;
   formato: 'chico' | 'mediano' | 'grande';
   key: string;
+  tabIndex?: number;
+  leerOrdinal?: boolean;
 }
 
-export default function BrailleSign({ caracter, formato }: BrailleSignProps) {
+export default function BrailleSign({ caracter, formato, tabIndex, leerOrdinal }: BrailleSignProps) {
   return (
-    <article id={caracter} className={`grid grid-rows-3 grid-cols-2 place-items-center cursor-pointer rounded
+    <div
+      tabIndex={caracter !== "º" ? tabIndex
+        : leerOrdinal === true ? tabIndex : undefined}
+      role="button"
+      aria-label={`
+        ${caracter === ' ' ? 'espacio'
+          : caracter === '.' ? 'punto'
+            : caracter === ':' ? 'dos puntos'
+              : caracter === ',' ? 'coma'
+                : caracter === ';' ? 'punto y coma'
+                  : caracter === '-' ? 'guión'
+                    : caracter === '¥' ? 'signo generador'
+                      : caracter === 'º' ? 'signo numérico'
+                        : caracter
+        }  
+      `}
+      id={caracter} className={`grid grid-rows-3 grid-cols-2 place-items-center cursor-pointer rounded
       bg-transparent hover:bg-gray-200 hover:bg-opacity-50 border border-black shadow-braille transition-colors duration-150
         ${formato === 'grande' ? 'w-[100px] h-[147px]' : ''}
         ${formato === 'mediano' ? 'w-[75px] h-[110.25px]' : ''}
         ${formato === 'chico' ? 'w-[50px] h-[73.5px]' : ''}
       `}>
-        {
-          brailleSignValue[caracter].map((punto, puntoIndex) => (
-            <span key={puntoIndex}
-              className={`${punto === 1 ? '' : 'bg-opacity-10'} bg-black rounded-full cursor-pointer shadow-braille
+      {
+        brailleSignValue[caracter].map((punto, puntoIndex) => (
+          <span key={puntoIndex} aria-hidden="true"
+            className={`${punto === 1 ? '' : 'bg-opacity-10'} bg-black rounded-full cursor-pointer shadow-braille
                 ${formato === 'grande' ? 'w-[30px] h-[30px]' : ''}
                 ${formato === 'mediano' ? 'w-[22.5px] h-[22.5px]' : ''}
                 ${formato === 'chico' ? 'w-[15px] h-[15px]' : ''}
             `}></span>
-          ))
-        }
-      </article>
+        ))
+      }
+    </div>
   )
 }

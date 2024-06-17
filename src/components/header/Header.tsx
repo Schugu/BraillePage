22b3dd69d404a860da535ea901ca943f582cd1e3
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-17 05:59:16 -0300

    Add: TabIndex del header/navbar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,14 +3,14 @@ import LinkSection from "./LinkSection.tsx";
 export default function Navbar() {
   return (
     <section className="flex flex-wrap items-center gap-2 justify-center md:justify-between p-2 
-    bg-blue-950 text-white text-xl">
-      <LinkSection LinkTo="/" Icon="w-9 h-9 icon-[cil--braille]" Label="BraillePage" />
+    bg-blue-950 text-white text-xl" id="header">
+      <LinkSection LinkTo="/" Icon="w-9 h-9 icon-[cil--braille]" Label="BraillePage" tabIndex={1} />
 
-      <LinkSection LinkTo="/alphabet" Icon="w-10 h-10 icon-[mdi--alphabetical-variant]" Label="El alfabeto" />
-      <LinkSection LinkTo="/numbers" Icon="w-10 h-10 icon-[mdi--numeric]" Label="Los números" />
-      <LinkSection LinkTo="/keyboard" Icon="w-8 h-8 icon-[mdi--keyboard-outline]" Label="Teclado" />
-      <LinkSection LinkTo="/wordsToBraille" Icon="w-8 h-8 icon-[arcticons--braille-de]" Label="Codificador de palabras" />
-      <LinkSection LinkTo="/numsToBraille" Icon="w-8 h-8 icon-[arcticons--braille-de]" Label="Codificador de números" />
+      <LinkSection LinkTo="/alphabet" Icon="w-10 h-10 icon-[mdi--alphabetical-variant]" Label="El alfabeto" tabIndex={2} />
+      <LinkSection LinkTo="/numbers" Icon="w-10 h-10 icon-[mdi--numeric]" Label="Los números" tabIndex={3} />
+      <LinkSection LinkTo="/keyboard" Icon="w-8 h-8 icon-[mdi--keyboard-outline]" Label="Teclado" tabIndex={4} />
+      <LinkSection LinkTo="/wordsToBraille" Icon="w-8 h-8 icon-[arcticons--braille-de]" Label="Codificador de palabras" tabIndex={5} />
+      <LinkSection LinkTo="/numsToBraille" Icon="w-8 h-8 icon-[arcticons--braille-de]" Label="Codificador de números" tabIndex={6} />
     </section>
   )
 }
--- src/components/header/LinkSection.tsx
+++ src/components/header/LinkSection.tsx
@@ -3,12 +3,13 @@ type LinkSectionProps = {
   LinkTo: string;
   Icon: string;
   Label: string;
+  tabIndex?: number;
 }
 
-export default function LinkSection({ LinkTo, Icon, Label }: LinkSectionProps) {
+export default function LinkSection({ LinkTo, Icon, Label, tabIndex }: LinkSectionProps) {
   const location = useLocation();
   return (
-    <Link to={LinkTo} 
+    <Link to={LinkTo} tabIndex={tabIndex}
     className={` ${location.pathname === LinkTo ? 'text-white' : 'text-gray-400' }
     flex gap-2 items-center`}>
       <span className={`${Icon}`} />

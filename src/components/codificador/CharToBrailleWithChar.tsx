85f6743fc93726daef1f994b9a55aa1591710174
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-17 11:19:39 -0300

    Add: tabIndex y aria-label

--- src/components/codificador/CharToBrailleWithChar.tsx
+++ src/components/codificador/CharToBrailleWithChar.tsx
@@ -4,16 +4,22 @@ import BrailleSign from "./BrailleSign.tsx";
 type CharToBrailleWithCharProps = {
   cadenaParaCodificar: string,
   formato: 'chico' | 'mediano' | 'grande';
+  indexProp: number;
 }
 
-export default function CharToBrailleWithChar({ cadenaParaCodificar, formato }: CharToBrailleWithCharProps) {
+export default function CharToBrailleWithChar({ cadenaParaCodificar, formato, indexProp }: CharToBrailleWithCharProps) {
   const cadenaSeparada = cadenaParaCodificar.toLowerCase().split("");
 
   const elementosBraille = cadenaSeparada.map((caracter, index) => {
     if (caracter in brailleSignValue) {
       return (
         <section key={`${caracter}-${index}`} className="flex flex-col gap-1">
-          <BrailleSign caracter={caracter} formato={formato} key={`braille-${caracter}-${index}`} />
+          <BrailleSign
+            tabIndex={indexProp + index}
+            caracter={caracter}
+            formato={formato}
+            key={`braille-${caracter}-${index}`}
+          />
 
           <article key={`article-${caracter}-${index}`} className={`flex justify-center items-center cursor-pointer rounded
             bg-transparent hover:bg-gray-200 hover:bg-opacity-50 border border-black shadow-braille transition-colors duration-150

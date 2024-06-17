a4f9e3983b64ddf9f92ccf36b8888bd323bd31a2
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-17 11:20:52 -0300

    Add: tabIndex y aria-label

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,22 +11,22 @@ export default function Home() {
 
       <article className="flex flex-col gap-8 md:gap-1">
         <div className="w-full flex flex-wrap items-center justify-center gap-2">
-          <CharsToBraille cadenaParaCodificar={"¥"} formato={resolution} />
+          <CharsToBraille cadenaParaCodificar={"¥"} formato={resolution} indexProp={9}/>
           <p tabIndex={8}
             className="inline-block max-w-lg text-center lg:text-left text-balance self-center"
           >El signo básico del Sistema se denomina SIGNO GENERADOR, y está formado por seis puntos.</p>
         </div>
 
         <div className="w-full flex flex-wrap items-center justify-center gap-2">
-          <CharsToBraille cadenaParaCodificar={"Felíz"} formato={resolution} />
-          <p tabIndex={9}
+          <CharsToBraille cadenaParaCodificar={"Felíz"} formato={resolution} indexProp={11}/>
+          <p tabIndex={10}
             className="inline-block max-w-lg text-center lg:text-left text-balance self-center"
           >Combinando de distintas maneras estos seis puntos, pueden representarse todas las letras, los números, los signos, la musicografía, etc.</p>
         </div>
 
         <div className="w-full flex flex-wrap items-center justify-center gap-2">
-          <CharToBrailleWithNumbers cadenaParaCodificar={"¥"} formato={resolution} />
-          <p tabIndex={10}
+          <CharToBrailleWithNumbers cadenaParaCodificar={"¥"} formato={resolution} indexProp={17}/>
+          <p tabIndex={16}
             className="inline-block max-w-lg text-center lg:text-left text-balance self-center"
           >Para facilitar la identificación de cada punto, cada uno, toma el nombre de un número.</p>
         </div>

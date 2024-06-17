04cc00fdadb6b536d80428d8a16fa9802eb3e85d
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-17 05:59:58 -0300

    Add: TabIndex del footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,17 +2,17 @@
 export default function Footer() {
   return (
     <footer className="flex flex-wrap justify-center gap-6 p-2 bg-blue-950 border-t-2 border-blue-500 text-white">
-      <a href="#"
+      <a href="#header" tabIndex={2024}
         className="flex justify-center items-end gap-2 hover:text-blue-400">
         <span className="w-8 h-8 icon-[bx--up-arrow-circle]"></span>
         <p className="text-2xl">Arriba</p>
       </a>
-      <a href="https://github.com/Schugu" target="_blank"
+      <a href="https://github.com/Schugu" target="_blank" tabIndex={2025}
         className="flex justify-center items-end gap-2 hover:text-blue-400">
         <span className="w-8 h-8 icon-[mdi--github]"></span>
         <p className="text-2xl">Github</p>
       </a>
-      <a href="https://www.linkedin.com/in/leandrodanielschugurensky/" target="_blank"
+      <a href="https://www.linkedin.com/in/leandrodanielschugurensky/" target="_blank" tabIndex={2026}
         className="flex justify-center items-end gap-2 hover:text-blue-400">
         <span className="w-8 h-8 icon-[mdi--linkedin]"></span>
         <p className="text-2xl">Linkedin</p>

be02910ec5faecaf48ba7f6c503f914cacb5ccac
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-17 11:20:45 -0300

    Add: tabIndex y aria-label

--- src/pages/Alphabet.tsx
+++ src/pages/Alphabet.tsx
@@ -9,7 +9,7 @@ export default function Alphabet() {
       <Titulo titulo="El alfabeto" />
 
       <article className="flex flex-col gap-2 items-center">
-        <CharToBrailleWithChar cadenaParaCodificar="abcdefghij" formato={resolution} />
+        <CharToBrailleWithChar cadenaParaCodificar="abcdefghij" formato={resolution} indexProp={9}/>
         <p tabIndex={8} className="text-center text-balanece w-3/4">
           Las primeras diez combinaciones, que representan las diez primeras letras del
           alfabeto, ocupan sólo los cuatro puntos superiores del signo generador.
@@ -18,8 +18,8 @@ export default function Alphabet() {
       </article>
 
       <article className="flex flex-col gap-2 items-center">
-        <CharToBrailleWithChar cadenaParaCodificar="klmnopqrst" formato={resolution} />
-        <p tabIndex={9} className="text-center text-balanece w-3/4">
+        <CharToBrailleWithChar cadenaParaCodificar="klmnopqrst" formato={resolution} indexProp={21}/>
+        <p tabIndex={20} className="text-center text-balanece w-3/4">
           Cada una de las siguientes diez combinaciones, agrega a las de la primera serie,
           el punto número 3. Este grupo de signos, se denomina SEGUNDA SERIE.
         </p>
@@ -27,16 +27,16 @@ export default function Alphabet() {
 
       <article className="flex flex-wrap justify-center gap-2">
         <div className="max-w-lg flex flex-col gap-2 items-center">
-          <CharToBrailleWithChar cadenaParaCodificar="uvxyz" formato={resolution} />
-          <p tabIndex={10} className="text-center text-balanece">
+          <CharToBrailleWithChar cadenaParaCodificar="uvxyz" formato={resolution} indexProp={31}/>
+          <p tabIndex={30} className="text-center text-balanece">
             Las siguientes cinco combinaciones, agregan el punto número 6, a las de la
             segunda serie. Este grupo de signos, se denomina TERCERA SERIE.
           </p>
         </div>
 
         <div className="max-w-64 flex flex-col gap-2 items-center">
-          <CharToBrailleWithChar cadenaParaCodificar="ñw" formato={resolution} />
-          <p tabIndex={11} className="text-center text-balanece">
+          <CharToBrailleWithChar cadenaParaCodificar="ñw" formato={resolution} indexProp={41}/>
+          <p tabIndex={40} className="text-center text-balanece">
             Estas dos letras, completan el alfabeto.
           </p>
         </div>
@@ -44,15 +44,15 @@ export default function Alphabet() {
 
       <article className="flex flex-wrap justify-center gap-8">
         <div className="max-w-lg flex flex-col gap-2 items-center">
-          <CharToBrailleWithChar cadenaParaCodificar="áéíóú" formato={resolution} />
-          <p tabIndex={12} className="text-center text-balanece">
+          <CharToBrailleWithChar cadenaParaCodificar="áéíóú" formato={resolution} indexProp={51}/>
+          <p tabIndex={50} className="text-center text-balanece">
             Vocales con acento.
           </p>
         </div>
 
         <div className="max-w-xl flex flex-col gap-2 items-center">
-          <CharToBrailleWithChar cadenaParaCodificar=" .,;:-" formato={resolution} />
-          <p tabIndex={13} className="text-center text-balanece">
+          <CharToBrailleWithChar cadenaParaCodificar=" .,;:-" formato={resolution} indexProp={61}/>
+          <p tabIndex={60} className="text-center text-balanece">
             Caracteres Especiales.
           </p>
         </div>

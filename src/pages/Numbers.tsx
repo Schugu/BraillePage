5f1828dff584c8ee13fe3b2147df8e51bfb9b3ba
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-17 06:30:37 -0300

    Add: TabIndex en numbers

--- src/pages/Numbers.tsx
+++ src/pages/Numbers.tsx
@@ -12,32 +12,29 @@ export default function Numbers() {
       
       <article className="flex flex-wrap gap-2 items-center justify-center">
         <CharsToBraille cadenaParaCodificar="º" formato={resolution} />
-        <p className="max-w-md text-center sm:text-left">Un signo formado por los puntos 3, 4, 5 y 6. A ese signo se lo denomina SIGNO DE NUMERO.</p>
+        <p tabIndex={8} className="max-w-md text-center sm:text-left">Un signo formado por los puntos 3, 4, 5 y 6. A ese signo se lo denomina SIGNO DE NUMERO.</p>
       </article>
 
       <article className="flex flex-col gap-2 items-center">
-        <p className="max-w-5xl text-center">Los dígitos se representan anteponiendo a las diez primeras letras del alfabeto.</p>
+        <p tabIndex={9} className="max-w-5xl text-center">Los dígitos se representan anteponiendo a las diez primeras letras del alfabeto.</p>
         <ANWWAN formato={resolution} />
       </article>
 
       <article className="w-full flex flex-col gap-6 items-center">
-        <p className="max-w-6xl text-center">Cuando se necesite escribir cantidades de mas de una cifra, se antepondrá sólo un signo de número a toda la cantidad, sin importar cuántas cifras que lleve.
+        <p tabIndex={10} className="max-w-6xl text-center">Cuando se necesite escribir cantidades de mas de una cifra, se antepondrá sólo un signo de número a toda la cantidad, sin importar cuántas cifras que lleve.
           El efecto del signo de número para convertir los signos de las letras, en números, termina al dejar un espacio en blanco.
         </p>
         <div className="w-full flex flex-wrap justify-center gap-4 md:gap-20">
           <section className="flex flex-col gap-2 items-center">
             <NumToBraille formato={resolution} numerosParaCodificar="26" />
-            <p>26</p>
+            <p tabIndex={11}>26</p>
           </section>
           <section className="flex flex-col gap-2 items-center">
             <NumToBraille formato={resolution} numerosParaCodificar="4053317" />
-            <p>4.053.317</p>
+            <p tabIndex={12}>4.053.317</p>
           </section>
         </div>
       </article>
-
-
-
     </section>
   )
 }

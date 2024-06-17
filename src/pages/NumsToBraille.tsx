f07d34a1bf90d95851246ac0840ce3056ca550c9
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-17 11:21:19 -0300

    Add: tabIndex y aria-label

--- src/pages/NumsToBraille.tsx
+++ src/pages/NumsToBraille.tsx
@@ -16,18 +16,22 @@ export default function NumsToBraille() {
       <Titulo titulo="Codificador de números"/>
 
       <article className="flex flex-col items-center gap-2">
-        <p className="text-center">Escriba un número</p>
+        <p tabIndex={8} className="text-center">Escriba un número</p>
         <div className="flex flex-wrap gap-2 justify-center">
           <input
+            tabIndex={9}
+            role="form"
             onChange={handleChange}
             value={numero}
             className="w-full rounded border-2 border-blue-400 p-2 outline-none"
             type="number"
-            placeholder="123"
+            placeholder="Ejemplo 123"
             autoComplete="off"
             min="0"
           />
           <button
+            tabIndex={10}
+            role="button"
             onClick={() => { setNumero('') }}
             className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
           >Borrar todo</button>
@@ -35,7 +39,7 @@ export default function NumsToBraille() {
       </article>
 
       <article className="w-full flex justify-center">
-        <NumToBraille numerosParaCodificar={numero} formato={resolution} />
+        <NumToBraille numerosParaCodificar={numero} formato={resolution} indexProp={11} leerOrdinal={true}/>
       </article>
     </section>
   );

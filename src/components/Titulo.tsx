3d82c8b9545d40982487bf61d8dc19f8350f6c86
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-06-17 05:59:42 -0300

    Add: TabIndex del home

--- src/components/Titulo.tsx
+++ src/components/Titulo.tsx
@@ -5,7 +5,7 @@ type tituloProp = {
 
 export default function Titulo({titulo}: tituloProp) {
   return (
-    <h1 className="w-full border-b-4 border-blue-950 text-3xl md:text-5xl text-center">
+    <h1 tabIndex={7} className="w-full border-b-4 border-blue-950 text-3xl md:text-5xl text-center">
       {titulo}
     </h1>
   )
